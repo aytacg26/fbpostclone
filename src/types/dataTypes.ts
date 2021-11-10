5b77992f09bcdde4ffe7ebd5d737eed0d01f2cfe
aytacguley <aytacguley@dklassix.com>
2021-11-10 22:04:33 +0200

    new TypeScript datatypes added for project

---
+++ src/types/dataTypes.ts
@@ -0,0 +1,23 @@
+export type image = {
+  id: string;
+  url: string;
+  title: string;
+  width: string;
+  height: string;
+  isPortraitImage?: boolean;
+};
+
+export type styleOption = {
+  imageContainerStyle: {
+    inset: string;
+  };
+  wrapperStyle: {
+    paddingTop: string;
+  };
+  rootStyle: {
+    height: string;
+    top?: string;
+    left?: string;
+    width: string;
+  };
+};
