d624d37a7c52405ad64faf325399438555621719
aytacguley <aytacguley@dklassix.com>
2021-11-11 22:57:10 +0200

    LinkLabel revised and additional props added

--- src/components/UI/LinkLabel/LinkLabel.tsx
+++ src/components/UI/LinkLabel/LinkLabel.tsx
@@ -1,11 +1,32 @@
 import classes from './LinkLabel.module.scss';
 
 interface IProps {
-  text?: string;
+  text?: string | string[];
+  top?: number | string;
+  right?: number | string;
+  bottom?: number | string;
+  left?: number | string;
 }
 
-const LinkLabel = ({ text }: IProps) => {
-  return <label className={classes.LinkLabel}>{text}</label>;
+const LinkLabel = ({ text, left, right, top, bottom }: IProps) => {
+  let content: any;
+  let isList = false;
+
+  if (typeof text === 'string') {
+    content = text;
+  } else {
+    isList = true;
+    content = text?.map((t) => <span key={t}>{t}</span>);
+  }
+
+  return (
+    <label
+      className={`${classes.LinkLabel} ${isList ? classes.List : ''}`}
+      style={{ top, right, bottom, left }}
+    >
+      {content}
+    </label>
+  );
 };
 
 export default LinkLabel;
