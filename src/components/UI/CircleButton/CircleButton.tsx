32232c89beffbd1f32e5b752270246efdd4be8ba
aytacguley <aytacguley@dklassix.com>
2021-11-12 21:38:38 +0200

    button label added to the CircleButton comp.

--- src/components/UI/CircleButton/CircleButton.tsx
+++ src/components/UI/CircleButton/CircleButton.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import { MdClear } from 'react-icons/md';
+import LinkLabel from '../LinkLabel/LinkLabel';
 import classes from './CircleButton.module.scss';
 
 interface IProps {
   width?: string | number;
   height?: string | number;
+  margin?: string;
   icon?: JSX.Element;
   onClick?: () => void;
   type?: 'button' | 'reset' | 'submit';
   backgroundColor?: string;
+  title?: string;
 }
 
 const CircleButton = ({
@@ -17,15 +21,31 @@ const CircleButton = ({
   onClick,
   type,
   backgroundColor,
+  title,
+  margin,
 }: IProps) => {
+  const [showLabel, setShowLabel] = useState(false);
+
+  const labelHandler = () => {
+    setShowLabel((prevState) => !prevState);
+  };
+
   return (
     <button
       className={classes.CircleButtonContainer}
-      style={{ width: width, height: height, backgroundColor: backgroundColor }}
+      style={{
+        width: width,
+        height: height,
+        backgroundColor: backgroundColor,
+        margin: margin,
+      }}
       onClick={onClick}
       type={type}
+      onMouseEnter={labelHandler}
+      onMouseLeave={labelHandler}
     >
       {icon ? icon : <MdClear />}
+      {showLabel && title && <LinkLabel text={title} bottom={-35} />}
     </button>
   );
 };
