ab17c797ef74fdb02e59b5e4d49621d77e079c3a
aytacguley <aytacguley@dklassix.com>
2021-11-13 20:42:14 +0200

    posts reducer added to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import uiReducer from './features/ui/uiSlice';
 import searchReducer from './features/search/searchSlice';
 import userReducer from './features/user/userSlice';
+import postReducer from './features/post/postSlice';
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     search: searchReducer,
     user: userReducer,
+    posts: postReducer,
   },
 });
 
