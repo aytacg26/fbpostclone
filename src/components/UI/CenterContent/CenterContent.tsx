4e386c56c6a5d23c9bb25cbd100abacab4737906
aytacguley <aytacguley@dklassix.com>
2021-11-13 20:13:45 +0200

    Posts moved to CenterContent Component

--- src/components/UI/CenterContent/CenterContent.tsx
+++ src/components/UI/CenterContent/CenterContent.tsx
@@ -1,5 +1,28 @@
+import { useAppSelector } from '../../../store/hooks';
+import StoryContainer from '../StoryContainer/StoryContainer';
+import PostPresentationWindow from '../PostPresentationWindow/PostPresentationWindow';
+import PostSelect from '../../Pages/Home/PostSelect/PostSelect';
+import classes from './CenterContent.module.scss';
+
 const CenterContent = () => {
-  return <div>Center Content</div>;
+  const { posts } = useAppSelector((state) => state.posts);
+
+  return (
+    <div className={classes.postsSection}>
+      <StoryContainer />
+      <PostSelect />
+      <div style={{ marginTop: '10px', maxHeight: '100%' }}>
+        {posts.map((post) => (
+          <PostPresentationWindow
+            user={post.createdBy}
+            post={post}
+            key={post.id}
+          />
+        ))}
+      </div>
+      <div className={classes.endOfSection}></div>
+    </div>
+  );
 };
 
 export default CenterContent;
