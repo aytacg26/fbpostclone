e66d2191046dc8601938198b1fa9d65108fa53d5
aytacguley <aytacguley@dklassix.com>
2021-11-13 20:40:27 +0200

    Post Statistics component takes data from global state

--- src/components/UI/PostPresentationWindow/PostStatistics/PostStatistics.tsx
+++ src/components/UI/PostPresentationWindow/PostStatistics/PostStatistics.tsx
@@ -3,81 +3,27 @@ import PostInteractionContainer from '../PostInteractionContainer/PostInteractio
 import PostRatings from '../PostRatings/PostRatings';
 import TextButton from '../../TextButton/TextButton';
 import classes from './PostStatistics.module.scss';
+import { IPost } from '../../../../interfaces/IPost';
 
-const userRatings = {
-  byUser: [
-    {
-      id: 'rating-0001',
-      userId: 'user-0001',
-      userName: 'Aytac Güley',
-      rateType: 'like',
-    },
-    {
-      id: 'rating-0002',
-      userId: 'user-0002',
-      userName: 'Aslı Güley',
-      rateType: 'like',
-    },
-    {
-      id: 'rating-0003',
-      userId: 'user-0003',
-      userName: 'Ziba Güley',
-      rateType: 'heart',
-    },
-    {
-      id: 'rating-0004',
-      userId: 'user-0004',
-      userName: 'Fatma Güley',
-      rateType: 'like',
-    },
-    {
-      id: 'rating-0005',
-      userId: 'user-0005',
-      userName: 'Halil Güley',
-      rateType: 'care',
-    },
-    {
-      id: 'rating-0006',
-      userId: 'user-0006',
-      userName: 'Mark Zuckerberg',
-      rateType: 'angry',
-    },
-    {
-      id: 'rating-0007',
-      userId: 'user-0007',
-      userName: 'Elon Musk',
-      rateType: 'haha',
-    },
-    {
-      id: 'rating-0008',
-      userId: 'user-0008',
-      userName: 'Bill Gates',
-      rateType: 'wow',
-    },
-    {
-      id: 'rating-0009',
-      userId: 'user-0009',
-      userName: 'Dan Abramov',
-      rateType: 'crying',
-    },
-  ],
-  totals: {
-    like: 3,
-    heart: 1,
-    care: 1,
-    angry: 1,
-    haha: 1,
-    wow: 1,
-    crying: 1,
-  },
-};
-
+interface IProps {
+  post: IPost;
+}
 //TODO : TextButton Label List does not work properly because of max-width of center section,
 //change the css structure of wall center!!!
 //TODO : Comments text button onClick handler will hide comments part of Post
-const PostStatistics = () => {
+const PostStatistics = ({ post }: IProps) => {
   const [showShareLabel, setShowShareLabel] = useState(false);
   const [leaveTimer, setLeaveTimer] = useState(false);
+  let labelList: string[] = [];
+
+  const numberOfComments = post.comments ? post.comments.length : 0;
+  const numberOfShares = post.shares ? post.shares.length : 0;
+
+  if (numberOfShares > 0 && post.shares) {
+    post.shares.forEach((share) => {
+      labelList.push(`${share.reactionBy.name} ${share.reactionBy.surname}`);
+    });
+  }
 
   const showLabelHandler = () => {
     setShowShareLabel(true);
@@ -108,26 +54,22 @@ const PostStatistics = () => {
     <PostInteractionContainer hasSpaceBetween>
       <PostRatings />
       <div>
-        <TextButton
-          buttonText={`${61} Comments`}
-          showLabel={false}
-          onClick={commentsSectionHandler}
-        />
-        <TextButton
-          buttonText={`${10} Shares`}
-          showLabel={showShareLabel}
-          onMouseEnter={showLabelHandler}
-          onMouseLeave={shareLeaveTimerHandler}
-          labelList={[
-            'Aytaç Güley',
-            'Aslı Güley',
-            'Ziba Güley',
-            'Fatma Güley',
-            'Halil Güley',
-            'Mark Zuckerberg',
-            'Gerald Hubert Irvin John Kenneth',
-          ]}
-        />
+        {numberOfComments > 0 && (
+          <TextButton
+            buttonText={`${numberOfComments} Comments`}
+            showLabel={false}
+            onClick={commentsSectionHandler}
+          />
+        )}
+        {numberOfShares > 0 && (
+          <TextButton
+            buttonText={`${numberOfShares} Shares`}
+            showLabel={showShareLabel}
+            onMouseEnter={showLabelHandler}
+            onMouseLeave={shareLeaveTimerHandler}
+            labelList={labelList}
+          />
+        )}
       </div>
     </PostInteractionContainer>
   );
