8998bc7ad0e8036ffe833bca5019d6fdc196a3af
aytacguley <aytacguley@dklassix.com>
2021-11-15 00:33:08 +0200

    Newly created UserCard component and functionality added to UserLabel

--- src/components/UI/UserLabel/UserLabel.tsx
+++ src/components/UI/UserLabel/UserLabel.tsx
@@ -1,9 +1,11 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import UserLabelAdditionalData from './UserLabelAdditionalData/UserLabelAdditionalData';
 import ProfileImage from '../ProfileImage/ProfileImage';
 import classes from './UserLabel.module.scss';
 import PropTypes from 'prop-types';
 import { actionBy, privacy } from '../../../types/dataTypes';
+import UserCard from './UserCard/UserCard';
 
 interface IProps {
   user: actionBy;
@@ -13,6 +15,7 @@ interface IProps {
   showFullName: boolean;
   hasBackground?: boolean;
   width?: string;
+  hasUserCard?: boolean;
 }
 
 //TODO : Add props to the component
@@ -27,14 +30,40 @@ const UserLabel = ({
   showFullName,
   hasBackground,
   width,
+  hasUserCard,
 }: IProps) => {
-  //const [showUserCard, setShowUserCard] = useState(false);
+  const [showUserCard, setShowUserCard] = useState(false);
+  const [startUserCardShow, setStartUserCardShow] = useState(false);
+
+  const userCardHandler = () => {
+    setStartUserCardShow((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    let timer: any;
+
+    if (startUserCardShow) {
+      timer = setTimeout(() => {
+        setShowUserCard(true);
+      }, 600);
+    } else {
+      timer = setTimeout(() => {
+        setShowUserCard(false);
+      }, 200);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [startUserCardShow]);
 
   return (
     <div
       className={`${classes.UserLabelContainer} ${
         hasBackground ? classes.buttonShape : ''
       }`}
+      onMouseEnter={userCardHandler}
+      onMouseLeave={userCardHandler}
     >
       <Link to={`/${user.id}`}>
         <ProfileImage
@@ -51,6 +80,7 @@ const UserLabel = ({
         showDateAndTime={showDateAndTime}
         showFullName={showFullName}
       />
+      {showUserCard && hasUserCard && <UserCard user={user} />}
     </div>
   );
 };
