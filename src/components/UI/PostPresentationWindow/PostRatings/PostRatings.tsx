0b0bb2fce3ea65c0459a36bbb654c2816cac502c
aytacguley <aytacguley@dklassix.com>
2021-11-14 20:06:03 +0200

    PostRatingEmojies added to PostRating component

--- src/components/UI/PostPresentationWindow/PostRatings/PostRatings.tsx
+++ src/components/UI/PostPresentationWindow/PostRatings/PostRatings.tsx
@@ -1,40 +1,12 @@
 import { useState } from 'react';
-import like from '../../../../assets/images/interactions/thumbsUp.svg';
-import heart from '../../../../assets/images/interactions/heart.svg';
-import care from '../../../../assets/images/interactions/care.svg';
-import haha from '../../../../assets/images/interactions/HaHa.svg';
-import wow from '../../../../assets/images/interactions/wow.svg';
-import crying from '../../../../assets/images/interactions/crying.svg';
-import angry from '../../../../assets/images/interactions/angry.svg';
 import { ILike } from '../../../../interfaces/IPost';
 import TextButton from '../../TextButton/TextButton';
-import MenuButton from '../../MenuButton/MenuButton';
+import PostRatingEmojies from '../PostRatingEmojies/PostRatingEmojies';
 import classes from './PostRatings.module.scss';
-import { likeType } from '../../../../types/dataTypes';
 
 interface IProps {
   likes?: ILike[];
 }
-
-const getLikeIconByType = (likeType: likeType) => {
-  switch (likeType) {
-    case 'wow':
-      return wow;
-    case 'heart':
-      return heart;
-    case 'haha':
-      return haha;
-    case 'crying':
-      return crying;
-    case 'angry':
-      return angry;
-    case 'care':
-      return care;
-
-    default:
-      return like;
-  }
-};
 
 const PostRatings = ({ likes }: IProps) => {
   const [showLikesList, setShowLikesList] = useState(false);
@@ -63,7 +35,7 @@ const PostRatings = ({ likes }: IProps) => {
   return (
     <div className={classes.RatingsContainer}>
       <div className={classes.Emojies}>
-        Emojies
+        <PostRatingEmojies likes={likes} />
         {/**Create emoji component which will show 7 different emoji upon typeCount of each (>=1), each emoji will have a LinkLabel */}
       </div>
       <div>
